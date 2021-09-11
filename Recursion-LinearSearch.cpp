3e4727fcbb48921645c0554d6d6f30ac79780cad
Akhileshsukumaran <akhileshs2017@gmail.com>
2021-09-11 10:52:55 +0530

    code update

--- Recursion-LinearSearch.cpp
+++ Recursion-LinearSearch.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-int Ls(int arr[], int n, int key)
+int FirstOccr(int arr[], int n, int key)
 {
     if(n == 0)
     {
@@ -14,7 +14,7 @@ int Ls(int arr[], int n, int key)
         return 0;
     }
 
-    int subIndex = Ls(arr+1, n-1, key);
+    int subIndex = FirstOccr(arr+1, n-1, key);
 
     if(subIndex != -1)
     {
@@ -49,14 +49,41 @@ int Ls(int arr[], int n, int key)
 }
 */
 
+int LastOccr(int arr[], int n, int key)
+{
+    if(n == 0)
+    {
+        return -1;
+    }
+
+    int subIndex = LastOccr(arr+1, n-1, key);
+    if(subIndex == -1)
+    {
+        if(arr[0] == key)
+        {
+            return 0;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        return subIndex + 1;
+    }
+}
+
 int main()
 {
-    int arr[] = {1, 3, 5, 7, 10, 4, 18};
+    int arr[] = {1, 3, 5, 7, 10, 4, 7, 18};
     int n = sizeof(arr)/sizeof(arr[0]);
     int key ;
     cout<<"enter the element to be searched: ";
     cin>>key;
-    cout<<Ls(arr, n-1, key)<<endl;
+    cout<<FirstOccr(arr, n-1, key)<<endl;
+
+    cout<<LastOccr(arr, n-1, key)<<endl;
 
     return 0;
 }
